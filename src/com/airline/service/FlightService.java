5b86c80b4f56365792c407003de500d0aff86d43
Ozaren <krishna.sd.2012@gmail.com>
2017-03-11 15:37:17 -0500

    Session 4 Lecture 22
    Added FlightService

---
+++ src/com/airline/service/FlightService.java
@@ -0,0 +1,79 @@
+package com.airline.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class FlightService
+ */
+@Stateless
+@LocalBean
+public class FlightService {
+	private int	id			= 999999;
+	private int	price		= 400;
+	private int	numOfSeats	= 250;
+	
+	private String	from = "Middle Earth";
+	private String	to = "Hyper Earth";
+	private String	airplaneModel = "Firebolt";
+	
+	/**
+	 * Default constructor.
+	 */
+	public FlightService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNumOfSeats() {
+		return numOfSeats;
+	}
+
+	public void setNumOfSeats(int numOfSeats) {
+		this.numOfSeats = numOfSeats;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getAirplaneModel() {
+		return airplaneModel;
+	}
+
+	public void setAirplaneModel(String airplaneModel) {
+		this.airplaneModel = airplaneModel;
+	}
+
+	@Override
+	public String toString() {
+		return "FlightService [id=" + id + ", price=" + price + ", numOfSeats=" + numOfSeats + ", from=" + from + ", to=" + to + ", airplaneModel=" + airplaneModel + "]";
+	}
+}
